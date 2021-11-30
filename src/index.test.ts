b0cf2d6a86a9e71cd861276f4e3443bbc1674bfb
charlesdobson <charlesdobson92@gmail.com>
2021-11-30 11:55:14 -0500

    feat: add test and serve, start, and test scripts

---
+++ src/index.test.ts
@@ -0,0 +1,9 @@
+import { start } from "./index";
+
+describe("start", () => {
+  it('Should log "Hello, World!"', () => {
+    const consoleSpy = jest.spyOn(console, "log");
+    start();
+    expect(consoleSpy).toHaveBeenCalledWith("Hello, World!");
+  });
+});
